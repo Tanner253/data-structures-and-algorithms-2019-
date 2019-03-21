854bf987215ce1a862695560932f96e9356e3056
Tanner <Percivaltanner@gmail.comgit add index.html$ git add index.htmlgit config --global user.Email Percivaltanner@gmail.com>
2019-03-21 11:08:14 -0700

    adds test

--- challenges/C#/BinarySearch/BinarySearch/Program.cs
+++ challenges/C#/BinarySearch/BinarySearch/Program.cs
@@ -1,34 +1,44 @@
 ﻿using System;
+using System.Globalization;
 using System.Runtime.InteropServices.WindowsRuntime;
 
 namespace BinarySearch
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             int[] array = { 1, 2, 3, 4, 5, 6, 7};
             Console.WriteLine("Hello World!");
+            int num = Selection();
             
-            BinarySearch(array);
+            Console.WriteLine(BinarySearchValid(array, num));
+        }
+
+        public static int Selection()
+        {
+            Console.WriteLine("choose a number");
+            string rawNum = Console.ReadLine();
+            int num = Convert.ToInt32(rawNum);
+           
+            return num;
         }
         /// <summary>
         /// runs conditionals to decide what output should be
         /// </summary>
         /// <param name="array">array of intigers the logic is run against</param>
         /// <returns>an index value or -1 for doesn't exist</returns>
-        public static int BinarySearch(int[] array)
+        
+        public static int BinarySearchValid(int[] array , int num)
         {
-            PrintOptions(array);
-            Console.WriteLine("choose a number");
-            string rawNum = Console.ReadLine();
-            int num = Convert.ToInt32(rawNum);
+
+            
             int start = 0;
             int end = array.Length ;
-            int index = (start + end) / 2;
             do
             {
 
+                int index = (start + end) / 2;
                 if (array[index] == num)
                 {
                     return index;
@@ -49,16 +59,8 @@ namespace BinarySearch
             return -1;
             
         }
-        /// <summary>
-        /// prints every value in the array to the console
-        /// </summary>
-        /// <param name="array">array of available ints</param>
-        public static void PrintOptions(int[] array)
-        {
-            foreach (int value in array)
-            {
-                Console.WriteLine(value);
-            }
-        }
+       
+       
+        
     }
 }
