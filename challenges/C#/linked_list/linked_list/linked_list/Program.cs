41dc7aaf7ddc8804a901ddcb91c5f34f0d038479
Tanner <Percivaltanner@gmail.comgit add index.html$ git add index.htmlgit config --global user.Email Percivaltanner@gmail.com>
2019-03-25 09:16:49 -0700

    adds linked list functionality (found on MDN as i have NO clue how to impliment this)

--- challenges/C#/linked_list/linked_list/linked_list/Program.cs
+++ challenges/C#/linked_list/linked_list/linked_list/Program.cs
@@ -1,12 +1,64 @@
 ﻿using System;
+using System.Net.Http.Headers;
+using System.Transactions;
 
 namespace linked_list
 {
     class Program
     {
+        public class Node
+        {
+            public Node next;
+            public int data;
+        }
+        public class LinkedList
+        {
+            private Node head;
+        public void PrintAllNodes()
+        {
+            Node current = head;
+            while(current != null)
+                {
+                    Console.WriteLine(current.data);
+                    current = current.next;
+                }
+            }
+        public void AddFirst(int data)
+        {
+                Node toAdd = new Node
+                {
+                    data = data,
+                    next = head
+                };
+            }
+            public void AddLast(int data)
+            {
+                if (head == null)
+                {
+                    head = new Node();
+                    head.data = data;
+                    head.next = null;
+
+                }
+                else
+                {
+                    Node toAdd = new Node();
+                    toAdd.data = data;
+                    Node current = head;
+                    while(current.next != null)
+                    {
+                        current = current.next;
+
+                    }
+                    current.next = toAdd;
+
+                }
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
         }
     }
 }
