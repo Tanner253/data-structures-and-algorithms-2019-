a852efad0865cfbc007d3c2affc6eee60f6ae806
Tanner <Percivaltanner@gmail.comgit add index.html$ git add index.htmlgit config --global user.Email Percivaltanner@gmail.com>
2019-04-09 07:18:18 -0700

    submits challenge

---
+++ challenges/C#/fizzBuzz-tree/FizzBuzz-Tree/FizzBuzz-Tree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FizzBuzz_Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
