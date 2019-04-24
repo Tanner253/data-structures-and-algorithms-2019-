8dd5caa4602f1310ec2f2650ca631ea9779aeb70
Tanner <Percivaltanner@gmail.comgit add index.html$ git add index.htmlgit config --global user.Email Percivaltanner@gmail.com>
2019-04-23 21:03:05 -0700

    submits open pr will finish tmr mornign for sumbission

---
+++ sorting/MergeSort/MergeSort/MergeSort/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MergeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static int MergeSort(int[] array)
+        {
+            int n = array.Length;
+            int mid = array.Length / 2;
+            int[] b = array[array < mid];
+        }
+    }
+}
